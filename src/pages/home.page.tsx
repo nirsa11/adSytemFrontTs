5134789923715963037306a0c9f42882c9950d20
nirsa11 <134936759+nirsa11@users.noreply.github.com>
2023-06-18 14:44:16 +0300

    dashboard(fix): navigate to edit company from homepage

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,5 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export const HomePage = () => {
-    return (
-        <div className="home"> Home Page</div>
-    )
-}
+  return (
+    <div className="home">
+      {' '}
+      <Link to="/dashboard/edit-profile">go to edit propfile</Link>
+    </div>
+  );
+};
